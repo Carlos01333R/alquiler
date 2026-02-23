f514ef68b865918efcaa974e05527d9ede9733dc
Carlos01333R <carlosrodriguezanavila0@gmail.com>
2026-02-22 19:37:10 -0500

    cambiar el nombre al login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -56,8 +56,8 @@ export default function Login() {
           <div className="bg-white/20 rounded-2xl p-4 inline-block mb-4">
           <Image src="/logo.jpg" alt="Logo" width={100} height={100} className="rounded-2xl" />
           </div>
-          <h1 className="text-2xl font-bold text-white mb-2">Sistema de Loterías</h1>
-          <p className="text-white/90 text-sm">Brasil & Colombia</p>
+          <h1 className="text-2xl font-bold text-white mb-2">Sistema de alquiles</h1>
+          <p className="text-white/90 text-sm">Somos la solución a tus necesidades</p>
         </div>
 
         {/* Formulario */}
